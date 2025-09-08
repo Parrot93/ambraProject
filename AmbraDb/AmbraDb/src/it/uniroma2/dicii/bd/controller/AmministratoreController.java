9f29eeff0d63b91554633aa8fa1da80b58745855
Parrot93 <m.pratesi@cyberethicslab.com>
2025-09-08 09:38:12 +0200

    Delete AmbraDb/AmbraDb directory
    aggiornamentoFileJava

--- AmbraDb/AmbraDb/src/it/uniroma2/dicii/bd/controller/AmministratoreController.java
+++
@@ -1,9 +0,0 @@
-package it.uniroma2.dicii.bd.controller;
-
-public class AmministratoreController implements Controller {
-
-    @Override
-    public void start() {
-        throw new RuntimeException("Not implemented yet");
-    }
-}
--- AmbraDb/AmbraDb/src/it/uniroma2/dicii/bd/controller/Controller.java
+++
@@ -1,6 +0,0 @@
-package it.uniroma2.dicii.bd.controller;
-
-public interface Controller {
-
-    void start();
-}
